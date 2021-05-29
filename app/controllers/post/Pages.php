4ebd33a268c7cf2c476f556f41217f58e15c416c
Prince Oduro <prince@getinnotized.com>
2021-05-29 09:08:43 +0000

    sms numbers

--- app/controllers/post/Pages.php
+++ app/controllers/post/Pages.php
@@ -168,12 +168,11 @@ class Pages extends PostController {
 
                 if ($pro->store()) {
                     if($catid == 15){
-                        // $telephone = '0544466770';
-                        $telephone = '0541105550';
+                        $telephone = '0559687988';
                     }elseif($catid == 16){
-                        $telephone = '0263200066';
+                        $telephone = '0558216152';
                     }elseif($catid == 17){
-                        $telephone = '0239979601';
+                        $telephone = '0243602118';
                     }
                     //$ownertelephone = '0243144908';
                     sendRestockSMS($telephone, $productname, $oldqty, $newquantity);
@@ -215,12 +214,11 @@ class Pages extends PostController {
 
                 if ($pro->store()) {
                     if($catid == 15){
-                        // $telephone = '0544466770';
-                        $telephone = '0541105550';
+                        $telephone = '0559687988';
                     }elseif($catid == 16){
-                        $telephone = '0263200066';
+                        $telephone = '0558216152';
                     }elseif($catid == 17){
-                        $telephone = '0239979601';
+                        $telephone = '0243602118';
                     }
                     //$ownertelephone = '0243144908';
                     sendRemovestockSMS($telephone, $productname, $oldqty, $newquantity);
@@ -262,12 +260,11 @@ class Pages extends PostController {
 
                 if ($pro->store()) {
                     if($catid == 15){
-                        // $telephone = '0544466770';
-                        $telephone = '0541105550';
+                        $telephone = '0559687988';
                     }elseif($catid == 16){
-                        $telephone = '0263200066';
+                        $telephone = '0558216152';
                     }elseif($catid == 17){
-                        $telephone = '0239979601';
+                        $telephone = '0243602118';
                     }
                     //$ownertelephone = '0243144908';
 
